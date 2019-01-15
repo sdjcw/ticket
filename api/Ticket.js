25b7d2adddfa0ba70697fe8879083ec58c17207e
wchen <sdjcw13@gmail.com>
2019-01-15 10:00:18 -0500

    Revert "Merge branch 'leancloud'"
    之前错误的将 leancloud 分支合并到 master，导致一些 LeanCloud 定制的特性
    被合并，现在回滚。
    This reverts commit 904b83ff9b22923aefb69f1bf06d27e66016bbfa, reversing
    changes made to f58812bf323cbb5115ebda124a3e22fa8165199c.

--- api/Ticket.js
+++ api/Ticket.js
@@ -3,7 +3,6 @@ const AV = require('leanengine')
 
 const common = require('./common')
 const {getTinyUserInfo, htmlify, getTinyReplyInfo} = common
-const oauth = require('./oauth')
 const notify = require('./notify')
 const {TICKET_STATUS, ticketClosedStatuses} = require('../lib/common')
 const errorHandler = require('./errorHandler')
@@ -12,25 +11,23 @@ AV.Cloud.beforeSave('Ticket', (req, res) => {
   if (!req.currentUser._sessionToken) {
     return res.error('noLogin')
   }
-  return oauth.checkPermission(req.currentUser)
-  .then(() => {
-    const ticket = req.object
-    if (!ticket.get('title') || ticket.get('title').trim().length === 0) {
-      throw new AV.Cloud.Error('title 不能为空')
-    }
-    if (!ticket.get('category') || !ticket.get('category').objectId) {
-      throw new AV.Cloud.Error('category 不能为空')
-    }
+  const ticket = req.object
+  if (!ticket.get('title') || ticket.get('title').trim().length === 0) {
+    throw new AV.Cloud.Error('title 不能为空')
+  }
 
-    ticket.set('status', TICKET_STATUS.NEW)
-    ticket.set('content_HTML', htmlify(ticket.get('content')))
-    ticket.set('author', req.currentUser)
-    return selectAssignee(ticket)
-    .then((assignee) => {
-      ticket.set('assignee', assignee)
-      res.success()
-      return
-    })
+  if (!ticket.get('category') || !ticket.get('category').objectId) {
+    throw new AV.Cloud.Error('category 不能为空')
+  }
+
+  ticket.set('status', TICKET_STATUS.NEW)
+  ticket.set('content_HTML', htmlify(ticket.get('content')))
+  ticket.set('author', req.currentUser)
+  return selectAssignee(ticket)
+  .then((assignee) => {
+    ticket.set('assignee', assignee)
+    res.success()
+    return
   }).catch((err) => {
     errorHandler.captureException(err)
     res.error(err)
--- api/notify.js
+++ api/notify.js
@@ -3,7 +3,6 @@ const AV = require('leanengine')
 
 const mail = require('./mail')
 const bearychat = require('./bearychat')
-const zulip = require('./zulip')
 const wechat = require('./wechat')
 
 const {TICKET_STATUS} = require('../lib/common')
@@ -13,7 +12,6 @@ exports.newTicket = (ticket, author, assignee) => {
   return Promise.all([
     mail.newTicket(ticket, author, assignee).catch(err => errorHandler.captureException(err)),
     bearychat.newTicket(ticket, author, assignee).catch(err => errorHandler.captureException(err)),
-    zulip.newTicket(ticket, author, assignee).catch(err => errorHandler.captureException(err)),
     wechat.newTicket(ticket, author, assignee).catch(err => errorHandler.captureException(err)),
   ])
 }
@@ -30,7 +28,6 @@ exports.replyTicket = (ticket, reply, replyAuthor) => {
   return Promise.all([
     mail.replyTicket(data).catch(err => errorHandler.captureException(err)),
     bearychat.replyTicket(data).catch(err => errorHandler.captureException(err)),
-    zulip.replyTicket(data).catch(err => errorHandler.captureException(err)),
     wechat.replyTicket(data).catch(err => errorHandler.captureException(err)),
   ])
 }
@@ -39,23 +36,18 @@ exports.changeAssignee = (ticket, operator, assignee) => {
   return Promise.all([
     mail.changeAssignee(ticket, operator, assignee).catch(err => errorHandler.captureException(err)),
     bearychat.changeAssignee(ticket, operator, assignee).catch(err => errorHandler.captureException(err)),
-    zulip.changeAssignee(ticket, operator, assignee).catch(err => errorHandler.captureException(err)),
     wechat.changeAssignee(ticket, operator, assignee).catch(err => errorHandler.captureException(err)),
   ])
 }
 
 exports.ticketEvaluation = (ticket, author, to) => {
-  return Promise.all([
-    bearychat.ticketEvaluation(ticket, author, to).catch(err => errorHandler.captureException(err)),
-    zulip.ticketEvaluation(ticket, author, to).catch(err => errorHandler.captureException(err)),
-  ])
+  return bearychat.ticketEvaluation(ticket, author, to)
 }
 
 const sendDelayNotify = (ticket, to) => {
   return Promise.all([
     mail.delayNotify(ticket, to).catch(err => errorHandler.captureException(err)),
     bearychat.delayNotify(ticket, to).catch(err => errorHandler.captureException(err)),
-    zulip.delayNotify(ticket, to).catch(err => errorHandler.captureException(err)),
     wechat.delayNotify(ticket, to).catch(err => errorHandler.captureException(err)),
   ])
 }
